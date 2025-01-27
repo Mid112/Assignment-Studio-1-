f57703823509411521307d04da8ca5c457c6e9f9
Mridul Nohria <mridulnohria2004@gmail.com>
2025-01-26 19:40:38 -0800

    Refactored Input Handling

--- Assets/TutorialInfo/BallController.cs
+++ Assets/TutorialInfo/BallController.cs
@@ -2,37 +2,13 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    public Rigidbody sphereRigitbody; 
-    public float ballSpeed = 2f;
+    [SerializeField] private Rigidbody sphereRigitbody; 
+    [SerializeField] private float ballSpeed = 2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    
+    public void MoveBall(Vector2 input)
     {
-        Debug.Log("Hello World!");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        Vector2 inputVector = Vector2.zero; // intialize our input vector 
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector += Vector2.up; // "a += b" <=> "a = a + b" 
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector += Vector2.down;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector += Vector2.right;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector += Vector2.left;
-        }
-        Vector3 inputXZPlane = new Vector3(inputVector.x, 0, inputVector.y); 
-        sphereRigitbody.AddForce(inputXZPlane*ballSpeed);
+        Vector3 inputXZPlane = new (input.x, 0, input.y);
+        sphereRigitbody.AddForce(inputXZPlane * ballSpeed);
     }
 }
